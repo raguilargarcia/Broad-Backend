68900e8c8eeb9d7ea666ca7858c0fba2131d6ef9
Roberto <ragugar2705@iesfuengirola1.es>
2025-03-27 14:46:21 +0100

    Añade el proyecto Broad al repositorio remoto

---
+++ Broad/src/main/java/com/broad/BroadApplication.java
@@ -0,0 +1,13 @@
+package com.broad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroadApplication.class, args);
+	}
+
+}
---
+++ Broad/src/test/java/com/broad/broad/BroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broad.broad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
